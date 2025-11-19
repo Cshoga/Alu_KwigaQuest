655a0b87ab7c006be242b5a5cade09a814391b6c
Celine SHOGA <c.shoga@alustudent.com>
2025-11-19 15:01:27 +0200

    Navbar component for site navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,15 +16,3 @@ export default function Navbar(){
     </div>
   )
 }
-
-export default function LessonCard({lesson, onMark}){
-  return (
-    <div className="w3-card card w3-margin-bottom">
-      <div className="w3-container">
-        <h3>{lesson.title}</h3>
-        <p>{lesson.content.length>250?lesson.content.slice(0,250)+"...":lesson.content}</p>
-        <button className="w3-button w3-blue w3-round" onClick={()=>onMark(lesson.id)}>Mark done</button>
-      </div>
-    </div>
-  )
-}
