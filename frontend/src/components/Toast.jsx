3f90d8dc099b2695a17d9e04f2284814ba50be6b
Celine SHOGA <c.shoga@alustudent.com>
2025-11-25 14:13:44 +0200

    displaying messages

---
+++ frontend/src/components/Toast.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function Toast({ message, type }){
+  if(!message) return null
+  return <div className={"toast " + (type === 'error' ? 'error' : '')}>{message}</div>
+}
