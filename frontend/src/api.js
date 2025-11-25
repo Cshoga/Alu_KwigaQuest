f0dbb19d68f72364f2e6336c986d8cd94be5ca13
Celine SHOGA <c.shoga@alustudent.com>
2025-11-25 08:44:22 +0200

    Delete all

--- frontend/src/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-const API = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000/api'
-const client = axios.create({baseURL:API})
-client.interceptors.request.use(cfg=>{const token = localStorage.getItem('token');if(token) cfg.headers.Authorization = 'Bearer '+token;return cfg})
-export default client
