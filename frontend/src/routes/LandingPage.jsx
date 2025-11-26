0b05e9c987efa7292e5a42e6910ba5538560d766
Celine SHOGA <c.shoga@alustudent.com>
2025-11-26 17:40:03 +0200

    Landing page

--- frontend/src/routes/LandingPage.jsx
+++ frontend/src/routes/LandingPage.jsx
@@ -3,10 +3,24 @@ import { useNavigate } from 'react-router-dom'
 
 export default function LandingPage(){
   const nav = useNavigate()
+
   return (
-    <div className='container center' style={{height:'60vh',flexDirection:'column'}}>
-      <h1 style={{fontSize:38,marginBottom:16}}>KwigaQuest</h1>
-      <button className='button' onClick={()=>nav('/login')}>Let's Get Started</button>
+    <div 
+      className="container center" 
+      style={{ height:'100vh', flexDirection:'column' }}
+    >
+      <h1 style={{ fontSize: '4rem', marginBottom: 20, fontWeight: 700 }}>
+        KwigaQuest
+      </h1>
+
+      <button 
+        className="button"
+        style={{ marginTop: 20 }}
+        onClick={() => nav('/login')}
+      >
+        Let's Get Started
+      </button>
     </div>
   )
 }
+
