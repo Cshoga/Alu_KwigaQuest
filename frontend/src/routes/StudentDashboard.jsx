a47a46a40d70944c8c34d6c3ca35a098050af9d4
Celine SHOGA <c.shoga@alustudent.com>
2025-11-28 15:42:02 +0200

    StudentDashboard

--- frontend/src/routes/StudentDashboard.jsx
+++ frontend/src/routes/StudentDashboard.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Sidebar from '../components/Sidebar'
 import Toast from '../components/Toast'
 import { sampleLessons, sampleQuizzes, sampleChallenges, sampleBadges } from '../data/sampleData'
 
